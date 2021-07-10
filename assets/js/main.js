f6fe4b2d9c2135e03ed20a01025341d51ae47592
Esteban Guzmán <57820858+ebanguz@users.noreply.github.com>
2021-07-10 18:23:14 -0400

    feature: localStorage
     Now functions works with localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,100 @@
 const d = document;
-d.addEventListener('DOMContentLoaded', (e) => {
-	const $list = d.querySelector('.list');
-	const $form = d.querySelector('form');
-	const $template = document.querySelector('#template').content;
-	const $fragmento = new DocumentFragment();
+const $list = d.querySelector('.list');
+const $form = d.querySelector('form');
+const $template = document.querySelector('#template').content;
+const $fragmento = new DocumentFragment();
+const $itemsLeft = d.getElementById('items-left');
 
-	const tasks = [
-		// {
-		// 	completed: false,
-		// 	task: 'progressing',
-		// },
-		// {
-		// 	completed: false,
-		// 	task: 'run',
-		// },
-		// {
-		// 	completed: true,
-		// 	task: 'eat',
-		// },
-	];
+const getData = () => {
+	const storage = localStorage.getItem('test');
+	const arrayOfTasks = storage ? JSON.parse(storage) : [];
+	return arrayOfTasks;
+};
 
-	//  RENDER FUNCTION
-	const renderTasks = () => {
-		$list.innerHTML = '';
+const tasks = getData();
+// console.table(tasks);
 
-		tasks.forEach((task, index) => {
-			const random = Math.floor(Math.random() * 100);
+//  RENDER FUNCTION
+const renderTasks = () => {
+	$list.innerHTML = '';
+	console.log('%c First List', 'font-weight: bold; color:red');
+	console.table(tasks);
 
-			$template.querySelector('input').setAttribute('id', random);
-			$template.querySelector('label').setAttribute('for', random);
-			$template.querySelector('label').textContent = task.task;
-			$template.querySelector('.delete-btn').dataset.id = index;
-
-			let clone = document.importNode($template, true);
-			$fragmento.appendChild(clone);
-		});
-		$list.appendChild($fragmento);
-	};
-
-	// RESET FORM FUNCTION
-	const resetForm = () => {
-		$form.reset();
-	};
-
-	// ADD FUNCTION
-	const addTask = (data) => {
-		const {completed, task} = data;
-		const entries = {
-			completed,
-			task,
-		};
-		tasks.push(entries);
-		renderTasks();
-	};
-
-	// DELETE FUNCTION
-	const deleteTask = (id) => {
-		tasks.splice(id, 1);
-		renderTasks();
-	};
-
-	// SUBMIT EVENT
-	d.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const formData = new FormData($form);
-		formData.set('completed', false);
-		const entries = formData.entries();
-		const data = Object.fromEntries(entries);
-
-		addTask(data);
-		resetForm();
+	tasks.forEach((task, index) => {
+		const status = task.completed;
+		$template.querySelector('input').checked = task.completed;
+		console.log(index + ' ' + task.completed);
+		$template.querySelector('input').setAttribute('id', index);
+		$template.querySelector('label').setAttribute('for', index);
+		$template.querySelector('label').textContent = task.task;
+		$template.querySelector('.delete-btn').dataset.id = index;
+		let clone = document.importNode($template, true);
+		$fragmento.appendChild(clone);
 	});
+	const strLength = tasks.length === 1 ? 'item' : 'items';
+	$itemsLeft.textContent = `${tasks.length} ${strLength} left`;
+	$list.appendChild($fragmento);
+};
 
-	// DELETE EVENT
-	d.addEventListener('click', (e) => {
-		if (e.target.matches('.delete-btn')) {
-			deleteTask(e.target.dataset.id);
-		}
-	});
+// ADD FUNCTION
+const addTask = (data) => {
+	const {completed, task} = data;
+	const entries = {
+		completed,
+		task,
+	};
+	tasks.push(entries);
+	localStorage.setItem('test', JSON.stringify(tasks));
 
-	// MAIN LOGIC
 	renderTasks();
+};
+
+// UPDATE
+const updateTask = (id) => {
+	const {completed, task} = tasks[id];
+
+	const entries = {
+		completed: !completed,
+		task,
+	};
+
+	tasks.splice(id, 1, entries);
+	localStorage.setItem('test', JSON.stringify(tasks));
+	renderTasks();
+};
+
+// DELETE FUNCTION
+const deleteTask = (id) => {
+	tasks.splice(id, 1);
+	localStorage.setItem('test', JSON.stringify(tasks));
+	renderTasks();
+};
+
+// SUBMIT EVENT
+d.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const formData = new FormData($form);
+	formData.set('completed', false);
+	const entries = formData.entries();
+	const data = Object.fromEntries(entries);
+
+	addTask(data);
+	$form.reset();
 });
+
+// DELETE EVENT
+d.addEventListener('click', (e) => {
+	if (e.target.matches('.delete-btn')) {
+		deleteTask(e.target.dataset.id);
+	}
+});
+
+d.addEventListener('change', (e) => {
+	if (e.target.matches('.chk')) {
+		console.table(tasks);
+		updateTask(e.target.id);
+	}
+});
+
+// MAIN LOGIC
+renderTasks();
