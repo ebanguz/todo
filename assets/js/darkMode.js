7b6b2259dfc762fd8da8e26e799be96d6e267203
Esteban Guzmán <57820858+ebanguz@users.noreply.github.com>
2021-07-12 14:17:52 -0400

    feauture: dark Mode v1 finish

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,17 +1,25 @@
 export default function darkMode(btn) {
 	const $btn = document.querySelector(btn);
-	// const elements = document.querySelectorAll('[data-dark]');
+	const $picture = document.querySelector('picture');
+
+	const printTheme = (theme) => {
+		$picture.innerHTML = `<source media="(max-width: 375px)" srcset="images/bg-mobile-${theme}.jpg" />
+			<source media="(min-width: 376px)" srcset="images/bg-desktop-${theme}.jpg" />
+			<img class="background__img" src="images/bg-mobile-${theme}.jpg" alt="background image ${theme}" />`;
+	};
 
 	let moon = `<img src="./images/icon-moon.svg" alt="toggler dark theme">`;
 	let sun = `<img src="./images/icon-sun.svg" alt="toggler light theme">`;
 
 	const dark = () => {
 		$btn.innerHTML = sun;
-		document.body.className = 'theme theme-dark';
+		printTheme('dark');
+		document.body.className = 'theme theme--dark';
 		localStorage.setItem('theme', 'dark');
 	};
 	const light = () => {
 		$btn.innerHTML = moon;
+		printTheme('light');
 		document.body.className = 'theme';
 		localStorage.setItem('theme', 'light');
 	};
@@ -19,7 +27,6 @@ export default function darkMode(btn) {
 	document.addEventListener('click', (e) => {
 		if (e.target.matches(btn) || e.target.matches(btn + ' img')) {
 			$btn.innerHTML === moon ? dark() : light();
-			console.log(document.body.className);
 		}
 	});
 
